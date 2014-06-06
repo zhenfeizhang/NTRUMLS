6137ee0dfb81f428a8b7751b3cb517088837ae9b
John M. Schanck <jschanck@securityinnovation.com>
2014-06-06 11:56:07 -0400

    Update tests

--- src/sanity.c
+++ src/sanity.c
@@ -33,7 +33,125 @@
 #include "pack.h"
 #include "pol.h"
 
-#define TIMES 1000
+#define TIMES 100
+
+static int
+testPack(PQ_PARAM_SET_ID id)
+{
+  int i;
+  int T;
+  int rc;
+  PQ_PARAM_SET *P;
+  if(!(P = pq_get_param_set_by_id(id)))
+  {
+    return -1;
+  }
+
+  unsigned char *scratch;
+  uint16_t *iF;
+  uint16_t *oF;
+  uint16_t *ig;
+  uint16_t *og;
+  int64_t *iginv;
+  int64_t *oginv;
+  int64_t *ih;
+  int64_t *oh;
+  int64_t *isig;
+  int64_t *osig;
+  unsigned char *priv_blob;
+  unsigned char *pub_blob;
+  unsigned char *sig_blob;
+
+  size_t prod = 2*(P->d1 + P->d2 + P->d3)*sizeof(uint16_t);
+  size_t full = P->N*sizeof(int64_t);
+  size_t priv_blob_len = PRIVKEY_PACKED_BYTES(P);
+  size_t pub_blob_len = PUBKEY_PACKED_BYTES(P);
+  size_t sig_len = SIGNATURE_BYTES(P);
+  size_t offset;
+
+  scratch = malloc(4*prod + 6*full + priv_blob_len + pub_blob_len + sig_len);
+
+  offset = 0;
+  iF = (uint16_t*)(scratch + offset); offset += prod;
+  oF = (uint16_t*)(scratch + offset); offset += prod;
+  ig = (uint16_t*)(scratch + offset); offset += prod;
+  og = (uint16_t*)(scratch + offset); offset += prod;
+  iginv = (int64_t*)(scratch + offset); offset += full;
+  oginv = (int64_t*)(scratch + offset); offset += full;
+  isig = (int64_t*)(scratch + offset); offset += full;
+  osig = (int64_t*)(scratch + offset); offset += full;
+  ih = (int64_t*)(scratch + offset); offset += full;
+  oh = (int64_t*)(scratch + offset); offset += full;
+  priv_blob = (unsigned char*)(scratch + offset); offset += priv_blob_len;
+  pub_blob = (unsigned char*)(scratch + offset); offset += pub_blob_len;
+  sig_blob = (unsigned char*)(scratch + offset); offset += sig_len;
+
+  for(T=0; T<TIMES; T++)
+  {
+    fastrandombytes(scratch, 4*prod + 6*full + priv_blob_len + pub_blob_len + sig_len);
+    for(i = 0; i < prod; i++)
+    {
+      iF[i] = iF[i] % P->N;
+      ig[i] = ig[i] % P->N;
+    }
+
+    for(i=0; i < P->N; i++)
+    {
+      iginv[i] = cmod(iginv[i], P->p);
+      ih[i] = cmod(ih[i], P->q);
+      isig[i] = isig[i] % P->q;
+      isig[i] = (isig[i] + P->q) % P->q;
+      isig[i] -= isig[i] % P->p;
+      isig[i] /= P->p;
+    }
+
+    rc = pack_private_key(P, iF, ig, iginv, priv_blob_len, priv_blob);
+    if(PQNTRU_ERROR == rc) { printf("Private key pack error\n"); return -1; }
+
+    rc = unpack_private_key(P, oF, og, oginv, priv_blob_len, priv_blob);
+    if(PQNTRU_ERROR == rc) { printf("Private key unpack error\n"); return -1; }
+
+    rc = pack_public_key(P, ih, pub_blob_len, pub_blob);
+    if(PQNTRU_ERROR == rc) { printf("Public key pack error\n"); return -1; }
+
+    rc = unpack_public_key(P, oh, pub_blob_len, pub_blob);
+    if(PQNTRU_ERROR == rc) { printf("Public key unpack error\n"); return -1; }
+
+    rc = pack_signature(P, isig, sig_len, sig_blob);
+    if(PQNTRU_ERROR == rc) { printf("Signature pack error\n"); return -1; }
+
+    rc = unpack_signature(P, osig, sig_len, sig_blob);
+    if(PQNTRU_ERROR == rc) { printf("Signature unpack error\n"); return -1; }
+
+    for(i=0; i<2*(P->d1 + P->d2 + P->d3); i++)
+    {
+      if(iF[i] != oF[i] || ig[i] != og[i])
+      {
+        printf("product form keys not equal\n");
+        break;
+      }
+    }
+
+    for(i=0; i<P->N; i++)
+    {
+      oh[i] = cmod(oh[i], P->q);
+      oginv[i] = cmod(oginv[i], P->p);
+      if(ih[i] != oh[i] || iginv[i] != oginv[i])
+      {
+        printf("%d %ld %ld %ld %ld\n", i, ih[i], oh[i], iginv[i], oginv[i]);
+        printf("public key or iginv not equal\n");
+        return -1;
+      }
+
+      if(isig[i] != osig[i])
+      {
+        printf("%d %ld %ld\n", i, isig[i], osig[i]);
+        printf("signatures not equal\n");
+        return -1;
+      }
+    }
+  }
+}
 
 static int
 testKeyGen(PQ_PARAM_SET_ID id)
@@ -51,25 +169,30 @@ testKeyGen(PQ_PARAM_SET_ID id)
   unsigned char *scratch;
   size_t         scratch_len;
 
-  uint16_t      *fi;
-  uint16_t      *gi;
-  int8_t        *ginv;
-  int64_t       *h;
+  int rc;
 
   if(!(P = pq_get_param_set_by_id(id)))
   {
     return -1;
   }
 
-  scratch_len = 6*POLYNOMIAL_BYTES(P);
+  size_t prod = 2*(P->d1 + P->d2 + P->d3)*sizeof(uint16_t);
+  size_t full = POLYNOMIAL_BYTES(P);
+  scratch_len = 2*prod + 6*full;
   scratch = malloc(scratch_len);
-  int64_t *a1 = (int64_t*)scratch;
-  int64_t *a2 = (int64_t*)(scratch + POLYNOMIAL_BYTES(P));
-  int64_t *a3 = (int64_t*)(scratch + 2*POLYNOMIAL_BYTES(P));
+  size_t offset = 0;
+  uint16_t *f = (uint16_t*)(scratch); offset += prod;
+  uint16_t *g = (uint16_t*)(scratch+offset); offset += prod;
+  int64_t *ginv = (int64_t*)(scratch+offset); offset += full;
+  int64_t *h = (int64_t*)(scratch+offset); offset += full;
+  int64_t *a1 = (int64_t*)(scratch+offset); offset += full;
+  int64_t *a2 = (int64_t*)(scratch+offset); offset += 3*full;
 
   for(i=0; i<TIMES; i++)
   {
-    memset(scratch, 0, 6 * POLYNOMIAL_BYTES(P));
+    memset(scratch, 0, scratch_len);
+
+    /* Generate a key */
     pq_gen_key(P, &privkey_blob_len, NULL, &pubkey_blob_len, NULL);
 
     privkey_blob = malloc(privkey_blob_len);
@@ -82,28 +205,30 @@ testKeyGen(PQ_PARAM_SET_ID id)
       fprintf(stderr, "\t fail in keygen\n");
     }
 
+    /* Unpack the key */
+    rc = unpack_private_key(P, f, g, ginv, privkey_blob_len, privkey_blob);
+    if(PQNTRU_ERROR == rc) { printf("Private key unpack error\n"); return -1; }
 
-    get_private_key_ptrs(NULL, &fi, &gi, &ginv, privkey_blob_len, privkey_blob);
-    get_public_key_ptrs(NULL, &h, NULL, pubkey_blob_len, pubkey_blob);
+    rc = unpack_public_key(P, h, pubkey_blob_len, pubkey_blob);
+    if(PQNTRU_ERROR == rc) { printf("Public key unpack error\n"); return -1; }
 
-    pol_mul_product(a1, h, P->d1, P->d2, P->d3, fi, P->N, a3);
+    /* Multiply h by f mod q, should have g in a1 */
+    pol_mul_product(a1, h, P->d1, P->d2, P->d3, f, P->N, a2);
     for(j=0; j<P->N; j++)
     {
       a1[j] = cmod(P->p * (h[j] + a1[j]), P->q);
     }
 
-    for(j=0; j<P->N; j++)
-    {
-      a2[j] = (int64_t) ginv[j];
-    }
-    pol_mul_coefficients(a3, a1, a2, P->N, P->padded_N, P->p, a3);
+    /* Multiply a1 by g inverse mod p, should have 1 in a2 */
+    pol_mul_coefficients(a2, a1, ginv, P->N, P->padded_N, P->p, a2);
     for(j=1; j<P->N; j++)
     {
-      if(a3[j] != 0)
+      if(a2[0] != 1 || a2[j] != 0)
       {
         fprintf(stderr, "\t bad key");
         free(privkey_blob);
         free(pubkey_blob);
+        free(scratch);
         return -1;
       }
     }
@@ -111,6 +236,7 @@ testKeyGen(PQ_PARAM_SET_ID id)
     free(privkey_blob);
     free(pubkey_blob);
   }
+  free(scratch);
 
   return 0;
 }
@@ -127,7 +253,8 @@ testSet(PQ_PARAM_SET_ID id)
   unsigned char *privkey_blob;
   unsigned char *pubkey_blob;
 
-  int64_t *sigs;
+  unsigned char *sigs;
+
   uint16_t msg_len = 256;
   unsigned char *msg;
 
@@ -145,7 +272,6 @@ testSet(PQ_PARAM_SET_ID id)
   privkey_blob = malloc(TIMES * privkey_blob_len);
   pubkey_blob = malloc(TIMES * pubkey_blob_len);
 
-  sigs = malloc(TIMES * P->N * sizeof(int64_t));
   msg = malloc(TIMES * msg_len * sizeof(int64_t));
   memset(msg, 0, TIMES*msg_len*sizeof(int64_t));
 
@@ -158,17 +284,21 @@ testSet(PQ_PARAM_SET_ID id)
     {
       result = -1;
       fprintf(stderr, "\t fail in keygen\n");
-      goto exit;
+      goto exit_kg;
     }
   }
+
+  size_t packed_sig_len;
+  pq_sign(&packed_sig_len, NULL, privkey_blob_len, privkey_blob, pubkey_blob_len, pubkey_blob, 0, NULL);
+  sigs = malloc(TIMES * packed_sig_len);
 
   for(i=0; i<TIMES; i++)
   {
     fastrandombytes(msg+(i*msg_len), msg_len);
-    if(PQNTRU_ERROR == pq_sign(sigs + (i * P->N),
+    if(PQNTRU_ERROR == pq_sign( &packed_sig_len, sigs + (i*packed_sig_len),
                                privkey_blob_len, privkey_blob + (i*privkey_blob_len),
-                               pubkey_blob_len, pubkey_blob + (i*pubkey_blob_len),
-                               msg_len, msg + (i*msg_len)))
+                                pubkey_blob_len, pubkey_blob + (i*pubkey_blob_len),
+                                        msg_len, msg + (i*msg_len)))
     {
       result = -1;
       fprintf(stderr, "\t fail in sign\n");
@@ -178,9 +308,9 @@ testSet(PQ_PARAM_SET_ID id)
 
   for(i=0; i<TIMES; i++)
   {
-    if(PQNTRU_ERROR == pq_verify(sigs + (i * P->N),
+    if(PQNTRU_ERROR == pq_verify( packed_sig_len, sigs + (i*packed_sig_len),
                                  pubkey_blob_len, pubkey_blob + (i*pubkey_blob_len),
-                                 msg_len, msg + (i*msg_len)))
+                                         msg_len, msg + (i*msg_len)))
     {
       result = -1;
       fprintf(stderr, "\t fail in verify\n");
@@ -191,8 +321,9 @@ testSet(PQ_PARAM_SET_ID id)
   fprintf(stderr, "\t good\n");
 
 exit:
-  free(msg);
   free(sigs);
+exit_kg:
+  free(msg);
   free(privkey_blob);
   free(pubkey_blob);
   return result;
@@ -208,6 +339,7 @@ main(int argc, char **argv)
 
   for(i = 0; i<numParams; i++)
   {
+    testPack(plist[i]);
     testKeyGen(plist[i]);
     testSet(plist[i]);
   }
